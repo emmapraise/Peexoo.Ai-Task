6b4c3d4f53c4c8ae98d9c8f0c93dab76d8cfb50e
Emmanuel Praise <emmapraise@gmail.com>
2020-03-17 09:05:08 +0100

    2nd commit

--- task1.py
+++ task1.py
@@ -1,10 +1,12 @@
 import cv2
 
-img = cv2.imread('ee.png')
+img = cv2.imread('ee.png', 0)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
 
-cv2.imshow('converted gray', gray)
+cv2.imshow('converted gray', img)
 
+cv2.imwrite('img/test.png', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
